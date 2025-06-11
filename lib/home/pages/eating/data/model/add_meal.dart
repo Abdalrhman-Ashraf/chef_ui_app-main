56fa03b0fccad3e22a871995d4085bfa7ed65365
Abdo <@user-xi6fb9wm3s>
2025-06-11 22:35:52 +0300

    model

--- lib/home/pages/eating/data/model/add_meal.dart
+++ lib/home/pages/eating/data/model/add_meal.dart
@@ -1,29 +1,43 @@
-
-
 class AddMeal {
-  String? name, description, price, offerPrice, image, preparationTime, categoryId, status;
+	String? name;
+	String? description;
+	String? price;
+	String? offerPrice;
+	String? image;
+	String? preparationTime;
+	String? categoryId;
+	String? status;
 
-  AddMeal({this.name, this.description, this.price, this.offerPrice, this.image, this.preparationTime, this.categoryId, this.status});
+	AddMeal({
+		this.name, 
+		this.description, 
+		this.price, 
+		this.offerPrice, 
+		this.image, 
+		this.preparationTime, 
+		this.categoryId, 
+		this.status, 
+	});
 
-  factory AddMeal.fromJson(Map<String, dynamic> json) => AddMeal(
-    name: json['name'],
-    description: json['description'],
-    price: json['price']?.toString(),
-    offerPrice: json['offer_price'],
-    image: json['image'],
-    preparationTime: json['preparation_time'],
-    categoryId: json['category_id'],
-    status: json['status'],
-  );
+	factory AddMeal.fromJson(Map<String, dynamic> json) => AddMeal(
+				name: json['name'] as String?,
+				description: json['description'] as String?,
+				price: json['price'] as String?,
+				offerPrice: json['offer_price'] as String?,
+				image: json['image'] as String?,
+				preparationTime: json['preparation_time'] as String?,
+				categoryId: json['category_id'] as String?,
+				status: json['status'] as String?,
+			);
 
-  Map<String, dynamic> toJson() => {
-    'name': name,
-    'description': description,
-    'price': price,
-    'offer_price': offerPrice,
-    'preparation_time': preparationTime,
-    'category_id': categoryId,
-    'status': status,
-    // EXCLUDE 'image': it’s/file later
-  };
+	Map<String, dynamic> toJson() => {
+				'name': name,
+				'description': description,
+				'price': price,
+				'offer_price': offerPrice,
+				'image': image,
+				'preparation_time': preparationTime,
+				'category_id': categoryId,
+				'status': status,
+			};
 }
